4dbb011b7c67dbf749eb2c507db421a998ab02ce
Abdullah Akbar <Akbar.A@etoncollege.org.uk>
2023-01-24 21:12:25 +0000

    Adding basic model for division and students.

---
+++ AbsenceRecorder/Models/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Akbar, Abdullah (RCH) on 24/01/2023.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    let students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
